5bb136730122922e8c75d833a65a68f14ac7d971
sgregoryy <160326604+sgregoryy@users.noreply.github.com>
2024-08-08 18:38:58 +0400

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,12 +1,12 @@
 import os
 
-BOT_TOKEN = '7236458100:AAEl9gpEFHkvkCBV0EKtaKHGs-FQfma0n-Q'
+BOT_TOKEN = ''
 PAYMENT_TOKEN = ""
 
-manager_url = "t.me/marssak"
-admin_chat = '1054184718'
+manager_url = ""
+admin_chat = ''
 
-username = 'alhim'
-password = '1234'
+username = ''
+password = ''
 host = ''
-database = 'shop'
+database = ''
